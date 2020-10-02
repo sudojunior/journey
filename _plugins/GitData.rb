3b2dd24203f77dbd79f3d2ec2a504db1f18ecda6
Junior <sudojunior@outlook.com>
2020-10-02 15:27:15 +0100

    Add base plugin file to request project data from GitHub

---
+++ _plugins/GitData.rb
@@ -0,0 +1,13 @@
+require "net/http"
+require "json"
+
+module Jekyll
+  safe false
+
+  Jekyll::Hooks.register :site, :after_init do |site|
+    repo = site.config["repository"]
+
+    uri = URI("https://api.github.com/repos/#{repo}")
+    site.config["github"] = JSON.parse(Net::HTTP.get(uri))
+  end
+end
