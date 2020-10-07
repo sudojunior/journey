4e02327a8ad17d8f562f64c4766ea5ee98287719
Junior <sudojunior@outlook.com>
2020-10-07 16:14:46 +0100

    Add data filters to category layout
    resolve #16

---
+++ _plugins/DataFIlters.rb
@@ -0,0 +1,29 @@
+module Jekyll
+  module DataFilter
+
+    def is_page(input)
+      for page in @context.registers[:site].pages
+        if input == page.url
+          return true
+        end
+      end
+      
+      return false
+    end
+
+    def match_path(url, *args)
+      match = true
+      url_parts = url.split "/"
+      url_parts.shift
+      args.map { |el|
+        index, part = el.split ":"
+        index = index.to_i
+        match = false if part != url_parts[index]
+      }
+      match
+    end
+
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::DataFilter)
